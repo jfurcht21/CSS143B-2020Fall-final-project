b36ce6711601ee9e233a8594218e0912dd351847
John <jfurcht21@gmail.com>
2020-12-06 11:57:44 -0800

    Resolves runtime errors. Next to resolve is return values error

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -8,24 +8,40 @@ import java.util.*;
 public class IndexerImpl implements Indexer {
     public Map<String, List<List<Integer>>> index(List<String> docs) {
         Map<String, List<List<Integer>>> indexes = new HashMap<>();
-        // add your code
+
+        //Loop through each document
         for(int i = 0; i < docs.size(); i++){
             String doc = docs.get(i);
+            List<List<Integer>> listCurrentDoc = new ArrayList<>();
+
+            //Split document 1 into array of individual words
             String[] words = doc.split(" ");
+
+            //Loop through array of words and insert each word at proper index
             for(int j = 0; j < words.length; j++){
+
+                //Removes whitespace or empty words
+                if(words[j] == " " || words[j] == ""){
+                    continue;
+                }
+
+                // What to do if word has already been found in document
+                // Add the word in an existing list and add the words location in the array (j)
                 if(indexes.containsKey(words[j])){
                     indexes.get(words[j]).add(Arrays.asList(i));
                 }
+
+                // If first time word is popping up in the document, create a new entry in the Map
                 else{
-                    List<List<Integer>> newList = new ArrayList<>();
                     List<Integer> newWord = new ArrayList<>();
-                    newList.add(newWord);
-                    //Fails here on i = 1 during indexer test. Also check to ensure document number is
-                    //Separate from list of positions in the document
-                    newList.set(i, Arrays.asList(j));
-                    indexes.put(words[j], newList);
+                    listCurrentDoc.add(newWord);
+                    listCurrentDoc.set(0, Arrays.asList(j));
+                    indexes.put(words[j], listCurrentDoc);
                 }
+
             }
+
+
         }
         return indexes;
     }
