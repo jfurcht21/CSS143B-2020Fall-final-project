c5795f157cde394f7681d1f586a4ea57603e9a2a
John <jfurcht21@gmail.com>
2020-12-06 17:43:25 -0800

    Stopping point. Commits changes

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -1,5 +1,6 @@
 package edu.uwb.css143b2020fall.service;
 
+import org.hibernate.cache.spi.StandardCacheTransactionSynchronization;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -8,6 +9,7 @@ import java.util.*;
 public class IndexerImpl implements Indexer {
     public Map<String, List<List<Integer>>> index(List<String> docs) {
         Map<String, List<List<Integer>>> indexes = new HashMap<>();
+        Map<int[], String> wordsInDoc = new HashMap<>();
 
         //Loop through each document
         for(int i = 0; i < docs.size(); i++){
@@ -17,7 +19,7 @@ public class IndexerImpl implements Indexer {
             //Split document 1 into array of individual words
             String[] words = doc.split(" ");
 
-            //Loop through array of words and insert each word at proper index
+            //Loop through array of words and insert each word into combined HashMap
             for(int j = 0; j < words.length; j++){
 
                 //Removes whitespace or empty words
@@ -25,23 +27,49 @@ public class IndexerImpl implements Indexer {
                     continue;
                 }
 
-                // What to do if word has already been found in document
-                // Add the word in an existing list and add the words location in the array (j)
-                if(indexes.containsKey(words[j])){
-                    indexes.get(words[j]).add(Arrays.asList(i));
-                }
-
-                // If first time word is popping up in the document, create a new entry in the Map
                 else{
-                    List<Integer> newWord = new ArrayList<>();
-                    listCurrentDoc.add(newWord);
-                    listCurrentDoc.set(0, Arrays.asList(j));
-                    indexes.put(words[j], listCurrentDoc);
+                    wordsInDoc.put(new int[]{i,j}, words[j]);
                 }
-
             }
 
+            for(Map.Entry<int[], String> entry : wordsInDoc.entrySet()){
+                int documentNumber = entry.getKey()[0];
+                int wordNumber = entry.getKey()[1];
+                String word = entry.getValue();
 
+                //Checks to see if word is already in indexes
+                if(indexes.containsKey(word)){
+                    if(indexes.get(word).get(documentNumber).size() == 0){
+                        indexes.get(word).set(documentNumber, Arrays.asList((wordNumber)));
+                    }
+                    else{
+                        List<Integer> tmp = indexes.get(word).get(documentNumber);
+                        ArrayList<Integer> wordsInDocument = new ArrayList<>(tmp.size());
+                        for(int l = 0; l < tmp.size(); l++){
+                         wordsInDocument.add((tmp.get(l)));
+                        }
+                        for(int l = 0; l < wordsInDocument.size(); l++){
+                            if(wordsInDocument.get(l) > wordNumber){
+                                wordsInDocument.add(l, wordNumber);
+                                break;
+                            }
+                            if(l == wordsInDocument.size() - 1){
+                                wordsInDocument.add(wordNumber);
+                                break;
+                            }
+                        }
+                        indexes.get(word).set(documentNumber, wordsInDocument);
+                    }
+                }
+                else{
+                    List<List<Integer>> newWord = new ArrayList<>(docs.size());
+                    for(int k = 0; k < docs.size(); k++){
+                        newWord.add(new ArrayList<>());
+                    }
+                    newWord.set(documentNumber, Arrays.asList(wordNumber));
+                    indexes.put(word, newWord);
+                }
+            }
         }
         return indexes;
     }
